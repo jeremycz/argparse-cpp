044aacf728e60864c7303cda41a3cd22f6b9f5a0
Jeremy Chen <jeremy.cz@gmail.com>
2021-10-16 20:02:50 +1300

    Create example executable

---
+++ example/build_example.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+g++ -std=c++17 -o example.app example.cpp
---
+++ example/example.cpp
@@ -0,0 +1,27 @@
+#include <chrono>
+#include <iostream>
+
+#include "../include/argparse.hpp"
+
+int main(int argc, char* argv[]) {
+    auto t0 = std::chrono::high_resolution_clock::now();
+
+    try {
+        ArgumentParser parser;
+        parser.AddArgument<float>("test");
+
+        auto args = parser.ParseArguments(argc, argv);
+
+        std::cout << args.GetArg<float>("test") << std::endl;
+    } catch (const std::exception& e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+
+    auto t1 = std::chrono::high_resolution_clock::now();
+    std::cout << "Time taken to process command line arguments: "
+              << std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0)
+                     .count()
+              << "us" << std::endl;
+
+    return 0;
+}
